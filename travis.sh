4692047db1eea7acab2b3e39a618c11e7cbbb9c5
Olivier Bonaventure <bonaventure@acm.org>
2019-04-05 13:03:59 +0000

    fix mscgen travis

--- travis.sh
+++ travis.sh
@@ -5,7 +5,7 @@ git clone https://github.com/sphinx-contrib/mscgen.git
 #wget https://files.pythonhosted.org/packages/69/2b/1d78898c62a9b12d0949d28c1221d4e7e299b7d51859b149f6f4e7b3ed51/sphinxcontrib-mscgen-0.4.tar.gz
 #tar xzvf sphinxcontrib-mscgen-0.4.tar.gz
 #cd sphinxcontrib-mscgen-0.4
-#cat sphinxcontrib/mscgen.py | sed 's/OSError,/OSError as/' > sphinxcontrib/mscgen.py
+cat mscgen/sphinxcontrib/mscgen.py | sed 's/OSError,/OSError as/' > mscgen/sphinxcontrib/mscgen.py
 #cat sphinxcontrib/mscgen.py | sed 's/from sphinx.util.compat import Directive/from docutils.parsers.rst import directives, Directive/' > sphinxcontrib/mscgen.py
 #cd ..
 pip3 install -e mscgen
